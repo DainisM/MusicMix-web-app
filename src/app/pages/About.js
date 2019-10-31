7e114fa90e29e008abac091e2ef520a39051be0c
Dainis DM. Moisejenko <damo@pc-tec.local>
2019-10-31 11:33:34 +0100

    Tweaked Home page and made About page

--- src/app/pages/About.js
+++ src/app/pages/About.js
@@ -1,9 +1,65 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
 
 const About = () => {
   return (
-    <div className="about">
-      <h3>About</h3>
+    <div>
+      <div className="container-fluid">
+        <img
+          src={require("../images/music-for-everyone.jpg")}
+          alt=""
+          width="100%"
+        />
+      </div>
+
+      <div className="container">
+        <br />
+        <h3>
+          Music for EVERYONE <em>literally</em>.
+        </h3>
+        <p>
+          Our main goal is to deliver good music to EVERYONE. With MusicMix, it
+          should be easy to find the right music every vibe.
+        </p>
+        <p>
+          Take a look at our <a href="/help">Help</a> page to find out
+          everything you need. After a peek even the guy from above could use
+          our MusicMix service flawlessly!
+        </p>
+        <p>
+          There are tons of tracks on MusicMix. So whether you’re behind the
+          wheel, working out, partying or relaxing, the right music is always
+          there for you.
+        </p>
+        <p>
+          Only thing you need to do is make a choise about what you want to
+          listen to.
+        </p>
+        <p>
+          MusicMix lets people discover and enjoy the greatest selection of
+          music from the hand-picked playlists by our Editors.
+        </p>
+        <br />
+        <h4>Contact:</h4>
+        <p>
+          <b>E-mail:</b> contact@music-mix.live
+        </p>
+        <h3>MusicMix HQ:</h3>
+        <p>Copenhagen, Denmark.</p>
+        <div>
+          <div>
+            <iframe
+              width="300px;"
+              height="250px;"
+              id="gmap_canvas"
+              src="https://maps.google.com/maps?q=Copenhagen%2C%20Denmark&t=&z10&ie=UTF8&iwloc=&output=embed"
+              frameBorder="0"
+              scrolling="no"
+            ></iframe>
+          </div>
+        </div>
+        <br />
+      </div>
     </div>
   );
 };
--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -12,81 +12,114 @@ class Home extends React.Component {
           <h3>Music for everyone</h3>
           <h4>Tons of different music for every vibe</h4>
         </div>
-
-        <div className="musicPlayer">
-          <div className="row playerRow">
-            <p>Jump in a world full of good music now</p>
-          </div>
-          <div className="row playerRow">
-            <button className="musicPlayerBtn " type="button">
-              Open Music Player
-            </button>
-          </div>
-        </div>
-
-        <div className="container information">
-          <div className="row">
-            <div className="col-lg-4 genres">
-              <h4>Many different genres</h4>
-              <img
-                className="genreImg"
-                src={require("../images/GenresMix.png")}
-                alt=""
-                width="256"
-                height="256"
-              />
-              <p>
-                Music sorted by genres for every taste, so everyone can find
-                what they like the most
-              </p>
-              <br />
+        <div className="content">
+          <div className="musicPlayer">
+            <div className="row playerRow">
+              <p>Jump in a world full of good music now</p>
             </div>
-            <div className="col-lg-4 collections">
-              <h4>Hand-picked playlists</h4>
-              <img
-                src={require("../images/playlists_cassettes.jpg")}
-                alt=""
-                width="256"
-                height="256"
-              />
-              <p>
-                Original playlists hand-picked by our editors, who are
-                constantly scouring the globe to find and make the best playlist
-                for every mood
-              </p>
-            </div>
-            <div className="col-lg-4 playlists">
-              <h4>Make your own collection</h4>
-              <img
-                src={require("../images/subwoofer.png")}
-                alt=""
-                width="256"
-                height="256"
-              />
-              <p>
-                Create your music library from many playlists, genres, mixes and
-                more to always enjoy yourself with good music
-              </p>
+            <div className="row playerRow">
+              <button className="musicPlayerBtn " type="button">
+                Open Music Player
+              </button>
             </div>
           </div>
-        </div>
 
-        <div className="signup-teaser">
-          <h3>Try MusicMix for free</h3>
-          <p>
-            Search for tracks, build your own playlists and listen to
-            pre-created playlists
-          </p>
-          <Link to="/signup">
-            <button className="signupLink" type="button">
-              Create account
-            </button>
-          </Link>
-          <div className="row login-teaser">
-            <p>Already have an account?</p>
-            <button className="loginLink" type="button">
-              Log in
-            </button>
+          <div className="container information">
+            <div className="row">
+              <div className="col-lg-4 genres">
+                <h4>Many different genres</h4>
+                <img
+                  className="genreImg"
+                  src={require("../images/GenresMix.png")}
+                  alt=""
+                  width="256"
+                  height="256"
+                />
+                <p>
+                  Music sorted by genres for every taste, so everyone can find
+                  what they like the most
+                </p>
+                <br />
+              </div>
+              <div className="col-lg-4 collections">
+                <h4>Hand-picked playlists</h4>
+                <img
+                  src={require("../images/playlists_cassettes.jpg")}
+                  alt=""
+                  width="256"
+                  height="256"
+                />
+                <p>
+                  Original playlists hand-picked by our editors, who are
+                  constantly scouring the globe to find and make the best
+                  playlist for every mood
+                </p>
+              </div>
+              <div className="col-lg-4 playlists">
+                <h4>Make your own collection</h4>
+                <img
+                  src={require("../images/subwoofer.png")}
+                  alt=""
+                  width="256"
+                  height="256"
+                />
+                <p>
+                  Create your music library from many playlists, genres, mixes
+                  and more to always enjoy yourself with good music
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="container-fluid listen-to-music">
+            <div className="row">
+              <div className="col-md-6">
+                <img
+                  className="listen-web-image"
+                  src={require("../images/listen-on-web.png")}
+                  alt=""
+                  width="490px"
+                  height="320"
+                />
+              </div>
+              <div className="col-md-6">
+                <h3>You bring the passion.</h3>
+                <h3>We bring the good vibe.</h3>
+                <h4>Listen to tons of good music here on web.</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="container news">
+            <h3>Comming soon...</h3>
+            <p>
+              MusicMix is rapidly growing and we are always trying to develop
+              new neat features for our users.
+            </p>
+            <p>
+              Our next big thing we are working on is mobile version of our
+              MusicMix Services.
+            </p>
+            <img src={require("../images/mobile-version.jpg")} alt="" />
+          </div>
+
+          <div className="signup-teaser">
+            <h3>Try MusicMix for free</h3>
+            <p>
+              Search for tracks, build your own playlists and listen to
+              pre-created playlists
+            </p>
+            <Link to="/signup">
+              <button className="signupLink" type="button">
+                Create account
+              </button>
+            </Link>
+            <div className="row login-teaser">
+              <p>Already have an account?</p>
+              <button className="loginLink" type="button">
+                Log in
+              </button>
+            </div>
           </div>
         </div>
       </div>
